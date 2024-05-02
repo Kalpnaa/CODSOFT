b5b27439cbbec10138fe3d6b72170550713c1568
Kalpnaa <167496655+Kalpnaa@users.noreply.github.com>
2024-05-02 09:10:36 +0530

    Add files via upload

---
+++ TASK1.cpp
@@ -0,0 +1,48 @@
+// NUMBER  GUESSING GAME
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+
+    int num;
+    srand(time(NULL));
+    num = rand() % 100 + 1;
+    int guess;
+    int count=0;
+
+    do
+    {
+        cout << "Enter the  number " << endl;
+        cin >> guess;
+
+        if (guess == num)
+        {
+            goto out;
+            count++;
+        }
+        else if (guess > num)
+        {
+            cout << "The number is too high" << endl;
+            count++;
+        }
+        else if (guess<num)
+        {
+            cout << "the number is too low" << endl;
+            count++;
+        }
+        else
+        {
+            cout<<"Invalid input"<<endl;
+        }
+
+    } while (guess != num);
+
+out:
+    cout << "your guessed in " << count << "attempt" << endl;
+
+    return 0;
+}
